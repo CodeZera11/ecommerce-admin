ef8584eea6c3e29993d773a6f9ef8bd01c086b04
Bhavesh Yadav <codezera3@gmail.com>
2023-08-03 19:12:44 +0530

    clerk authentication successfull

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-
-export default function Home() {
-  return (
-    <Button className='p-4 m-10' variant={"destructive"} >Click me</Button>
-  )
-}
